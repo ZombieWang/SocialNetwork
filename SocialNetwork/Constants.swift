205f69ef7b7a02d0aa956da79de99373bbe11f2e
Frank <frank7300228@gmail.com>
2017-03-04 17:42:49 +0800

    sign in ui custom complete

---
+++ SocialNetwork/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialNetwork
+//
+//  Created by Frank on 2017/3/4.
+//  Copyright © 2017年 Frank. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ SocialNetwork/RoundButton.swift
@@ -0,0 +1,28 @@
+//
+//  RoundButton.swift
+//  SocialNetwork
+//
+//  Created by Frank on 2017/3/4.
+//  Copyright © 2017年 Frank. All rights reserved.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        imageView?.contentMode = .scaleAspectFit
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2
+    }
+}
